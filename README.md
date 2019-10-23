def68c37900883b1e1b2a2e196d70a4cddd24584
danielhorsley <danielhorsley@users.noreply.github.com>
2019-10-23 09:53:20 +0100

    pop corrections

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 generate world using pop_db() in the room_populator.py in test_app
 **note this must be done from teh manage.py shell or it won't work
+
+on heroku /makeworld for creating new rooms db and makeitems for new items db
--- test_app/views.py
+++ test_app/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from test_app.room_populator import *
 
 # Create your views here.
 def startuptest(request):
     output = 'this is a test'
+    return HttpResponse(output)
+
+def makeworld(request):
+    pop_db()
+    output = 'world created - please check graphql for details'
+    return HttpResponse(output)
+
+def makeitems(request):
+    pop_items()
+    output = 'items created - please check graphql for details'
     return HttpResponse(output)
 
