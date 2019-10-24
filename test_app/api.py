78cd610319811b680e183476410b8159b2af8a07
danielhorsley <danielhorsley@users.noreply.github.com>
2019-10-24 14:13:14 +0100

    small correction

--- test_app/api.py
+++ test_app/api.py
@@ -73,6 +73,7 @@ def say(request):
 def look(request):
     player = request.user.player
     room = player.room()
+    players = room.playerNames(player_id)
     # IMPLEMENT
     return JsonResponse({'name':player.user.username, 
                             'title':room.title, 
