3ac243b40c8cc4a48c42f6d96e417aaf67994791
PWalis <pwilky1016@gmail.com>
2019-10-24 13:43:14 -0700

    Atempting to deploy to heroku again

--- tester/settings.py
+++ tester/settings.py
@@ -135,8 +135,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 GRAPHENE = {
     'SCHEMA': 'test_app.schema.schema' 
